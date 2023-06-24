8086c09b1e8eb0eabb9091a03ac2a45e22e0ebc6
Justin Isaac <kbulkiski@gmail.com>
2023-06-24 18:42:27 -0400

    Map

--- Canamaps/App.js
+++ Canamaps/App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
+  const [pin, setPin] = React.useState({
+    latitude: 37.78825,
+    longitude: -122.4324
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.View}>
+      <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,}}
+      >
+
+        <Marker coordinate={pin}
+          pinColor='blue'
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("Drag start", e.nativeEvent.coordinate.latitude)
+          }}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude
+            })
+          }}
+        >
+          <Callout>
+            <Text>Smoked here</Text>
+          </Callout>
+        </Marker>
+        <Circle center={pin}
+          radius={1000}/>
+      </MapView>
     </View>
   );
 }
@@ -13,8 +45,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
