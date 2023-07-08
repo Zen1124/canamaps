74d2cdf1aa3dd052ed803a7e013899ec43698c86
russellhermoso <russ6771@gmail.com>
2023-07-08 17:15:54 -0400

    Update Settings.js

--- Canamaps/Settings.js
+++ Canamaps/Settings.js
@@ -1,26 +1,66 @@
-import { useState, useEffect } from 'react';
-import MapView, { Callout, Circle, Marker } from 'react-native-maps';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
-import * as Location from 'expo-location';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Profile from './Profile';
-import Friends from './Friends';
-import { AntDesign } from '@expo/vector-icons';
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text, Switch, StyleSheet } from 'react-native';
 
+export default function Settings() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-export default function Settings () {
-
-    return (
-        <View>
-          <Text>Settings Screen</Text>
-          {/* Add your settings screen content here */}
-        </View>
-      );
-    
+  return (
+    <View style={[styles.container, isDarkMode && styles.darkModeContainer]}>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Settings</Text>
+      <View style={styles.switchContainer}>
+        <Text style={[styles.switchLabel, isDarkMode && styles.darkModeText]}>Dark Mode</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isDarkMode ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleDarkMode}
+          value={isDarkMode}
+        />
+      </View>
+      <Text style={[styles.modeText, isDarkMode && styles.darkModeText]}>
+        {isDarkMode ? 'Dark Mode' : 'Light Mode'}
+      </Text>
+    </View>
+  );
 }
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  darkModeContainer: {
+    backgroundColor: 'black',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'black',
+  },
+  darkModeText: {
+    color: 'white',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  switchLabel: {
+    marginRight: 10,
+    fontSize: 16,
+    color: 'black',
+  },
+  modeText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: 'black',
+  },
+});
